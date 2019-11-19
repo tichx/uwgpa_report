75f8cc2e2c4e6e258728abdd992a98208c812f0d
Yuheng Zhong <lyuheng13@gmail.com>
2019-11-18 21:46:54 -0800

    new

--- scripts/summary.R
+++ scripts/summary.R
@@ -1,7 +1,7 @@
 library(dplyr)
 
 # import dataset
-df <- read.csv(file = "../data/uw_courses.csv", stringsAsFactors = F)
+df <- read.csv(file = "data/uw_courses.csv", stringsAsFactors = F)
 
   
 # For this report, we are only concerned with INFO department
@@ -69,13 +69,13 @@ get_summary <- function(dataframe) {
     pull()
   
   
-    summary <- list("min_gpa" = lowest_gpa,
-            "ax_withdrawlfail" = most_wf,
-            "largest_class" = biggest_class,
-            "best_performing_class" = most_four_course,
-            "best_prof" = highest_rated_prof)
+    summary <- list(min_gpa = lowest_gpa,
+            ax_withdrawlfail = most_wf,
+            largest_class = biggest_class,
+            best_performing_class = most_four_course,
+            best_prof = highest_rated_prof)
   return(summary)
 }
 
-summary <- get_summary(df)
+summa <- get_summary(df)
   
