90256555e4cba267c6d51b9e5f96a72c69c42bbe
Keyan Ding <dingk4@uw.edu>
2019-11-18 20:49:02 -0800

    revise scatterplot

--- scripts/summary_table.R
+++ scripts/summary_table.R
@@ -9,9 +9,9 @@ df <- read.csv("data/uw_courses.csv", stringsAsFactors = FALSE)
 #Popularity of classes depends on the total enrollment of that certain class. 
 #More Popular class will have more student enrollment than less popular class. 
 
-P_table <- df %>%
-  filter(dept_abbrev == "INFO") %>%
-  mutate(course_tag = paste(dept_abbrev, course_no)) %>%
-  group_by(course_tag, course_title) %>%
-  summarize(class_popularity = sum(as.double(student_count))) %>%
-  arrange(-class_popularity)
+  P_table <- df %>%
+    filter(dept_abbrev == "INFO") %>%
+    mutate(course_tag = paste(dept_abbrev, course_no)) %>%
+    group_by(course_tag, course_title) %>%
+    summarize(class_popularity = sum(as.double(student_count))) %>%
+    arrange(-class_popularity)
