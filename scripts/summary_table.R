7549f1319f4c09f3006edaea5853ab893c806bc4
Keyan Ding <dingk4@uw.edu>
2019-11-18 23:12:09 -0800

    fix table

--- scripts/summary_table.R
+++ scripts/summary_table.R
@@ -1,5 +1,6 @@
 #Install package
 library("dplyr")
+library(knitr)
 
 #Import data set
 df <- read.csv("data/uw_courses.csv", stringsAsFactors = FALSE)
@@ -8,13 +9,19 @@ df <- read.csv("data/uw_courses.csv", stringsAsFactors = FALSE)
 #We want to summarize the popularity of informatics classes
 #Popularity of classes depends on the total enrollment of that certain class. 
 #More Popular class will have more student enrollment than less popular class. 
+summ_table <- function(df) {
+  p_table <- df %>%
+    filter(dept_abbrev == "INFO") %>%
+    mutate(course_tag = paste(dept_abbrev, course_no)) %>%
+    group_by(course_tag, course_title) %>%
+    summarize(class_popularity = sum(as.double(student_count))) %>%
+    arrange(-class_popularity)
+  return(kable(p_table, col.names = c("Course", "Title", "Class Popularity")))
+}
 
-P_table <- df %>%
-  filter(dept_abbrev == "INFO") %>%
-  mutate(course_tag = paste(dept_abbrev, course_no)) %>%
-  group_by(course_tag, course_title) %>%
-  summarize(class_popularity = sum(as.double(student_count))) %>%
-  arrange(-class_popularity)
+table <- summ_table(df)
+
+
 
 
 
